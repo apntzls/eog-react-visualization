e0c4d019d59c209014c0bb782804abba68e53c8b
Alex Pantzialas <APntzls@Gmail.com>
2019-08-08 11:49:57 -0400

    Edit to MetricsToggler

--- src/components/MetricsToggler.js
+++ src/components/MetricsToggler.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Switch from "@material-ui/core/Switch";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -6,9 +6,9 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 import Paper from "@material-ui/core/Paper";
 import { useDispatch, useSelector } from "react-redux";
 import { Provider, createClient, useQuery } from "urql";
-import { capitalize } from "lodash";
 import { makeStyles } from "@material-ui/core/styles";
 import * as actions from "../store/actions";
+import { camelCaseToLabel } from "../utils";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -25,10 +25,10 @@ const client = createClient({
 });
 
 const query = `
-  query {
-    getMetrics
-  }
-  `;
+query {
+  getMetrics
+}
+`;
 
 export default () => {
   return (
@@ -38,16 +38,20 @@ export default () => {
   );
 };
 
-const getMetricsList = state => state.metrics.list;
-const getMetricsState = state => state.metrics.values;
+const getMetricsState = state => state.metrics;
 
 const Toggler = () => {
   const classes = useStyles();
   const [result] = useQuery({ query });
   const { fetching, data, error } = result;
   const dispatch = useDispatch();
-  const metricsList = useSelector(getMetricsList);
+  const [metrics, setMetrics] = useState([]);
   const metricsState = useSelector(getMetricsState);
+
+  const handleChange = name => event => {
+    const toggledMetric = { [name]: event.target.checked };
+    dispatch({ type: actions.TOGGLE_METRIC, toggledMetric });
+  };
 
   useEffect(() => {
     if (error) {
@@ -57,26 +61,15 @@ const Toggler = () => {
     if (!data) return;
     const { getMetrics } = data;
     dispatch({ type: actions.METRICS_LIST_RECEIVED, getMetrics });
+    setMetrics(getMetrics);
   }, [dispatch, data, error]);
 
   if (fetching) return <LinearProgress />;
 
-  const handleChange = name => event => {
-    const payload = { [name]: event.target.checked };
-    dispatch({ type: actions.TOGGLE_METRIC, payload });
-  };
-
-  const camelCaseToLabel = str => {
-    return str
-      .split(/(?=[A-Z])/)
-      .map(capitalize)
-      .join(" ");
-  };
-
   return (
     <Paper className={classes.paper}>
       <FormGroup className={classes.formGroup} row>
-        {metricsList.map(metric => (
+        {metrics.map(metric => (
           <FormControlLabel
             key={metric}
             control={
